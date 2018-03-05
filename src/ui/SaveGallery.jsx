58968b94cbc4f9b73a26f29f38cb36e37113a23d
Caleb Miller <calebdotmiller@gmail.com>
2018-03-04 22:48:49 -0500

    Various gallery improvements

--- src/ui/SaveGallery.jsx
+++ src/ui/SaveGallery.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import IconButton from 'material-ui/IconButton';
+import DeleteIcon from 'material-ui/svg-icons/action/delete';
+import EditIcon from 'material-ui/svg-icons/editor/mode-edit';
 
 import { makeGradientFromSave } from 'helpers/gradientHelpers';
 import {
@@ -44,6 +47,23 @@ export default connect(mapStateToProps, mapDispatchToProps)(SaveGallery);
 
 
 
+const commonButtonStyle = {
+	position: 'absolute',
+	zIndex: 1,
+	top: -3
+};
+
+const editButtonStyle = {
+	...commonButtonStyle,
+	right: 30
+};
+
+const deleteButtonStyle = {
+	...commonButtonStyle,
+	right: -16
+};
+
+
 class GradientPreview extends React.PureComponent {
 
 	constructor(props) {
@@ -69,11 +89,22 @@ class GradientPreview extends React.PureComponent {
 		};
 
 		return (
-			<div className="saveGallery__item">
-				<div className="saveGallery__item__gradient" style={gradientStyle} onClick={this.load} />
-				<button className="saveGallery__item__delete" onClick={this.delete}>
-					&times;
-				</button>
+			<div className="saveGallery__item fade-slide-up">
+				<div className="saveGallery__item__gradient" style={gradientStyle} />
+				<IconButton
+					className="saveGallery__item__edit"
+					onClick={this.load}
+					style={editButtonStyle}
+				>
+					<EditIcon color="#888" hoverColor="#ccc" />
+				</IconButton>
+				<IconButton
+					className="saveGallery__item__delete"
+					onClick={this.delete}
+					style={deleteButtonStyle}
+				>
+					<DeleteIcon color="#db0025" hoverColor="#8a0017" />
+				</IconButton>
 			</div>
 		);
 	}
